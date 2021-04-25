67daa6eb8ed9646ae423a27c976f045538ffe2d8
rasulkurbanov <rasuljohn139@gmail.com>
2021-04-26 01:02:12 +0500

    Add sending sms via twilio free trial account

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 from twilio.rest import Client
 
+account_sid = "AC9bb055dc733d087dcec1f91c9393b961"
+auth_token = "38c400707b3b1433b599c51a9ed38754"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    from_='+14153196148',
+    body="Assalomu alaykum Islomjon.Salomatmisiz?",
+    to='+998993241123'
+)
+
+print(message.sid)
